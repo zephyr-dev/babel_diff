4b368227a820e4d996bfb806bb9ad863ba58fff8
Noah Gordon and Ryan McGarvey <zephyr-eng+noah+ryan@gust.com>
2015-02-11 13:57:14 -0500

    Adds test for YamlDiffer and test yaml files

--- lib/babel_diff/yaml_differ.rb
+++ lib/babel_diff/yaml_differ.rb
@@ -1,5 +1,35 @@
 module BabelDiff
-  class YamlDiffer
+  class YamlDiffer < Struct.new(:current_version, :previous_version)
+    def updates
+      current_version_hash.each do |k, v|
+        if v.is_a? Hash
 
+        else
+
+        end
+      end
+    end
+
+    def additions
+
+    end
+
+    private
+
+    def updates_hash
+      @updated_hash ||= {}
+    end
+
+    def additions_hash
+      @additions_hash ||= {}
+    end
+
+    def previous_version_hash
+      YAML.load(previous_version)
+    end
+
+    def current_version_hash
+      YAML.load(current_version)
+    end
   end
 end
--- spec/babel_diff/yaml_differ_spec.rb
+++ spec/babel_diff/yaml_differ_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
-describe BabelDiff::FileHandler do
-  
+describe BabelDiff::YamlDiffer do
+  let(:current_version) { File.read("spec/test_files/updated_file.yml") }
+  let(:previous_version) { File.read("spec/test_files/seed.yml") }
+  subject(:differ) { BabelDiff::YamlDiffer.new(current_version, previous_version) }
+
+  describe '#updates' do
+    let(:expected_updates) { File.read("spec/test_files/expected_updates.yml")}
+    it "returns a yaml string representing the updated keys" do
+      expect(differ.updates).to eq(expected_updates)
+    end
+  end
+
+  describe '#additions' do
+    let(:expected_additions) { File.read("spec/test_files/expected_additions.yml") }
+    it "returns a yaml string representing the new keys" do
+      expect(differ.additions).to eq(expected_additions)
+    end
+  end
 end
