3c54684dc49ed18abb9b9a2947d0e3b0596a881b
Noah Gordon and Ryan McGarvey <zephyr-eng+noah+ryan@gust.com>
2015-02-11 17:24:37 -0500

    Optimizes YamlDiffer by flattening hashes and comparing via single keys

--- lib/babel_diff/version.rb
+++ lib/babel_diff/version.rb
@@ -1,3 +1,3 @@
 module BabelDiff
-  VERSION = "1.0.2"
+  VERSION = "1.0.3"
 end
--- lib/babel_diff/yaml_differ.rb
+++ lib/babel_diff/yaml_differ.rb
@@ -1,64 +1,49 @@
 require 'yaml'
-require 'pry'
 
 module BabelDiff
   class YamlDiffer < Struct.new(:current_version, :previous_version)
     def updates
-      process_difference(current_version_hash) unless @processed
+      process_difference unless @processed
       @processed = true
-      updates_hash.to_yaml
+
+      unflatten(updates_hash).to_yaml
     end
 
     def additions
-      process_difference(current_version_hash) unless @processed
+      process_difference unless @processed
       @processed = true
-      additions_hash.to_yaml
+
+      unflatten(additions_hash).to_yaml
     end
 
-    def process_difference(current_hash, keys = [])
-      current_hash.each do |key, value|
-        keys << key
-        if value.is_a? Hash
-          process_difference(value, keys)
-        else
-          case in_previous_version(keys, value)
-          when :does_not_exist
-            add_key_to_additions(keys, value)
-          when :value_different
-            add_key_to_updates(keys, value)
-          end
+    def process_difference
+      current = HashFlattener.new(current_version_hash).flatten
+      previous = HashFlattener.new(previous_version_hash).flatten
+
+      current.each do |k,v|
+        if ! previous.has_key?(k)
+          additions_hash[k] = v
+        elsif previous[k] != v
+          updates_hash[k] = v
         end
-        keys.pop
       end
-    end
-
-    def in_previous_version(keys, value)
-      current_value = previous_version_hash
-      keys.each do |key|
-        current_value = current_value[key]
-        return :does_not_exist if current_value.nil?
-      end
-      return :value_different if current_value != value
-    end
-
-    def add_key_to_updates(keys, value)
-      current_value = updates_hash
-      keys[0...-1].each do |key|
-        current_value = current_value[key] ||= {}
-      end
-      current_value[keys.last] = value
-    end
-
-    def add_key_to_additions(keys, value)
-      current_value = additions_hash
-      keys[0...-1].each do |key|
-        current_value = current_value[key] ||= {}
-      end
-      current_value[keys.last] = value
     end
 
 
     private
+
+    def unflatten(hash)
+      {}.tap do |unflattened_hash|
+        hash.each do |k,v|
+          keys = k.split(".")
+          current_hash = unflattened_hash
+
+          keys[0...-1].each { |key| current_hash = current_hash[key] ||= {} }
+
+          current_hash[keys.last] = v
+        end
+      end
+    end
 
     def updates_hash
       @updated_hash ||= {}
@@ -74,6 +59,26 @@ module BabelDiff
 
     def current_version_hash
       YAML.load(current_version) || {}
+    end
+  end
+
+  class HashFlattener < Struct.new(:hash)
+
+    def flatten(current_hash = hash, keys = [])
+      current_hash.each do |key, value|
+        new_keys = keys.dup << key
+        if value.is_a? Hash
+          flatten(value, new_keys)
+        else
+          flat_hash[new_keys.join(".")] = value
+        end
+      end
+
+      flat_hash
+    end
+
+    def flat_hash
+      @_flat_hash ||= {}
     end
   end
 end
